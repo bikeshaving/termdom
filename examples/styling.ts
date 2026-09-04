349f0143c7360225274b61e87134a6f8cb5475b0
Brian Kim <briankimpossible@gmail.com>
2026-09-04 17:35:59 -0400

    tweak

--- examples/styling.ts
+++ examples/styling.ts
@@ -17,9 +17,9 @@ document.head.innerHTML = `
 
 document.body.innerHTML = `
   <p>A paragraph made gray by a CSS rule.</p>
-  <p class="warm">A class makes the next paragraph yellow.</p>
+  <p class="warm">A class makes this paragraph yellow.</p>
   <p class="warm hot">Two classes beat one: red and bold.</p>
-  <p class="warm hot" style="color: green">An inline style beats all the rules.</p>
+  <p class="warm hot" style="color: green">An inline style beats all rules.</p>
   <p>Text can be <b>bold</b>, <i>italic</i>, <a>underlined</a> or <del>struck through</del>.</p>
   <p><span class="badge">A background</span> paints its cells.</p>
 `;
