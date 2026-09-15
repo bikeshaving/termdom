1c2ca277b2a154904182f9b48dfc5755830f61d3
brainkim <brainkim@users.noreply.github.com>
2026-09-15 20:05:41 +0000

    deploy: b3e2cede610377a6c93cd775a6cea4b3d727c2b3

--- static/examples-e591ca30dce490f3.js
+++ static/examples-e591ca30dce490f3.js
@@ -268,7 +268,10 @@ ${r.map(({variableName:i,uniqueLocalName:s})=>`  reactHotLoader.register(${i}, "
 		content: none;
 	}
 
+	/* A line is \`<code>text</code><br>\`, one line box; the site's block
+	   \`pre code\` would put the break on a line of its own. */
 	& code {
+		display: inline;
 		background: none;
 		border: none;
 		border-radius: 0;
